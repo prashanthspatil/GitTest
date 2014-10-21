c67c2815bca8bb6e1e2abe70de3e5134a7aeb6a1
Prashanth <Prashanth@Prashanths-MacBook-Pro.local>
2014-10-21 22:17:38 +0530

    second commit

--- GitLearn/ViewController.m
+++ GitLearn/ViewController.m
@@ -19,6 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     //first test comment
+    
+    //second test comment
 }
 
 - (void)didReceiveMemoryWarning {
