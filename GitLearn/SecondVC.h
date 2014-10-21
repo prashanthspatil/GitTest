a8b7f3624d402a7ff1aa8304081f56e3b52a8a2d
Prashanth <Prashanth@Prashanths-MacBook-Pro.local>
2014-10-21 22:40:04 +0530

    Adding new classes

---
+++ GitLearn/SecondVC.h
@@ -0,0 +1,13 @@
+//
+//  SecondVC.h
+//  GitLearn
+//
+//  Created by Prashanth on 21/10/14.
+//  Copyright (c) 2014 Prashanth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondVC : UIViewController
+
+@end
